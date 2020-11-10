8cfda79b980e2f7a71d51ed1aff0c0f6bdf5ca3f
Clara-Franzoni <franzoniclara23@gmail.com>
2020-11-10 14:57:45 -0300

    10/11/2020

--- src/components/MisReservasComponent.vue
+++ src/components/MisReservasComponent.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md" style="max-width: 350px">
     <q-list bordered separator>
       <q-item clickable v-ripple
-      v-for="reserva in reservas"
+      v-for="reserva in reserv"
       :key="reserva.nombre">
         <q-item-section>
           <q-item-label overline>{{reserva.nombre}}</q-item-label>
@@ -13,30 +13,33 @@
     <button @click="deletePublicacion">
   Eliminar publicacion
   </button>
-  <h3>Importe a Pagar: {{ sumaTotal() }} </h3>
+  <h4>Ganancia estimada: {{ gananciaTotal() }} </h4>
   </div>
 
 </template>
 
 <script>
+import { ELIMINAR_RESERVA } from '../store/reserva/types'
 import { mapGetters } from 'vuex'
 export default {
   data () {
     return {
-      sumaTotal
+      sumaTotal: 0,
+      reservas: []
     }
   },
   computed: {
     ...mapGetters({
-      reservas: 'reserva/listaReservas'
+      reserv: 'reserva/listaReservas'
     })
   },
   methods: {
     deletePublicacion: function (index) {
       this.$store.dispatch('reserva/' + ELIMINAR_RESERVA)
     },
-    sumaTotal: function () {
-      array.forEach(reservas => total += reservas.price)
+    gananciaTotal: function () {
+      this.reserv.forEach((reserva) => { this.sumaTotal += this.reserv.price })
+      return this.sumaTotal
     }
   }
 }
